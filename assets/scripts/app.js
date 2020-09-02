f153f31a75f98d96db02332ccfeec7a97b4b1bc3
tostos <saittosun2@gmail.com>
2020-09-02 11:54:07 +0200

    controlling the backdrop

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,11 +7,24 @@ const addMovieModal = document.getElementById("add-modal");
 const startAddMovieButton = document.querySelector("header button");
 // const startAddMovieButton = document.querySelector("header").lastElementChild;
 
+// const backdrop = document.body.firstElementChild;
+const backdrop = document.getElementById("backdrop");
+const cancelMovieButton = addMovieModal.querySelector(".btn--passive");
 
+const toggleBackdrop = () => {
+  backdrop.classList.toggle("visible");
+};
 
 // you can define a function whichever way you want with the function keyword and the declaration approach, with the expression approach where you stored a function in a constant or with the expression approach where you use an arrow function.
 const toggleMovieModal = () => {
   addMovieModal.classList.toggle("visible");
+  toggleBackdrop();
+};
+
+const cancelAddMovie = () => {
+  toggleMovieModal();
 };
 
 startAddMovieButton.addEventListener("click", toggleMovieModal);
+backdrop.addEventListener("click", toggleMovieModal);
+cancelMovieButton.addEventListener("click", cancelAddMovie);
