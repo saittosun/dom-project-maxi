2413b23b0c3b1de0cee061388dea0421c3fe677b
tostos <saittosun2@gmail.com>
2020-09-02 09:05:04 +0200

    selecting the modal and add button

---
+++ assets/scripts/app.js
@@ -0,0 +1,8 @@
+// jshint esversion: 9
+//  better approach because of performance
+const addMovieModal = document.getElementById("add-modal");
+// const addMovieModal = document.querySelector("#add-modal");
+// const addMovieModal = document.body.children[1];
+
+const startAddMovieButton = document.querySelector("header button");
+// const startAddMovieButton = document.querySelector("header").lastElementChild;
