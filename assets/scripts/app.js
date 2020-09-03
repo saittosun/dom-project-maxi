9d007c85e1f93c01647d653e5b937308f116201b
tostos <saittosun2@gmail.com>
2020-09-03 10:06:24 +0200

    fetching and validating user input

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -10,6 +10,9 @@ const startAddMovieButton = document.querySelector("header button");
 // const backdrop = document.body.firstElementChild;
 const backdrop = document.getElementById("backdrop");
 const cancelMovieButton = addMovieModal.querySelector(".btn--passive");
+const confirmAddMovieButton = cancelMovieButton.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll("input");
+// const userInputs = addMovieModal.getElementsByTagName("input");
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle("visible");
@@ -21,10 +24,38 @@ const toggleMovieModal = () => {
   toggleBackdrop();
 };
 
-const cancelAddMovie = () => {
+const cancelAddMovieHandler = () => {
   toggleMovieModal();
 };
 
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
+
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  if (
+    titleValue.trim() === "" ||
+    imageUrlValue.trim() === "" ||
+    ratingValue.trim() === "" ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert("please enter valid values (rating between 1 and 5)");
+  }
+
+  // bu refactor edilmis hali
+  // for (const el of userInputs) {
+  //     if (el.value.trim() === "" || +el.value > 5 || +el.value < 1) {
+  //       alert("warn");
+  //     }
+  // }
+};
+
 startAddMovieButton.addEventListener("click", toggleMovieModal);
-backdrop.addEventListener("click", toggleMovieModal);
-cancelMovieButton.addEventListener("click", cancelAddMovie);
+backdrop.addEventListener("click", backdropClickHandler);
+cancelMovieButton.addEventListener("click", cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener("click", addMovieHandler);
